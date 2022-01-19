501c099bd2eba222d25ace2a5149fb0027553122
Apurva <abadithe@caltech.edu>
2022-01-18 16:18:04 -0800

    add files

---
+++ code/run_nuscenes.py
@@ -0,0 +1,152 @@
+## Apurva Badithela
+# File to run nuscenes scenarios
+import os
+import pdb
+from utils import *
+dataroot="/Users/apurvabadithela/Documents/software/nuscenes/data/sets/nuscenes"
+from nuscenes.nuscenes import NuScenes, NuScenesExplorer
+from yolo_bboxes import *
+from shapely.geometry import MultiPoint, box
+
+nusc = NuScenes(dataroot="/Users/apurvabadithela/Documents/software/nuscenes/data/sets/nuscenes/")
+nms_thresh = 0.6
+iou_thresh = 0.4
+
+sensor = 'CAM_FRONT' # Sensor rleevant in this study
+PLOT = True
+
+# Helper function only to read front camera annotations:
+# Get annotations only on camera front:
+def is_cam_front_ann_token(anntoken: str,
+                      margin: float = 10,
+                      view: np.ndarray = np.eye(4),
+                      box_vis_level: BoxVisibility = BoxVisibility.ANY,
+                      out_path: str = None,
+                      extra_info: bool = False) -> None:
+    """
+    Render selected annotation.
+    :param anntoken: Sample_annotation token.
+    :param margin: How many meters in each direction to include in LIDAR view.
+    :param view: LIDAR view point.
+    :param box_vis_level: If sample_data is an image, this sets required visibility for boxes.
+    :param out_path: Optional path to save the rendered figure to disk.
+    :param extra_info: Whether to render extra information below camera view.
+    """
+    ann_record = nusc.get('sample_annotation', anntoken)
+    sample_record = nusc.get('sample', ann_record['sample_token'])
+    assert 'LIDAR_TOP' in sample_record['data'].keys(), 'Error: No LIDAR_TOP in data, unable to render.'
+
+    # Figure out which camera the object is fully visible in (this may return nothing).
+    boxes, cam = [], []
+    cams = [key for key in sample_record['data'].keys() if 'CAM' in key]
+    for cam in cams:
+        _, boxes, _ = nusc.get_sample_data(sample_record['data'][cam], box_vis_level=box_vis_level,
+                                                selected_anntokens=[anntoken])
+        if len(boxes) > 0:
+            break  # We found an image that matches. Let's abort.
+    assert len(boxes) > 0, 'Error: Could not find image where annotation is visible. ' \
+                           'Try using e.g. BoxVisibility.ANY.'
+    assert len(boxes) < 2, 'Error: Found multiple annotations. Something is wrong!'
+
+    cam = sample_record['data'][cam]
+    view = nusc.get('sample_data', cam)['channel']
+    if view == 'CAM_FRONT':
+        return True
+    else:
+        return False
+
+# Function to get yOlO Boxes:
+def get_yolo_boxes(yolo, data_path):
+    boxes, boxes_pixels_yolo = yolo.yolo(data_path)
+    return boxes, boxes_pixels_yolo
+
+# function to get nuscenes boxes:
+def get_gt_boxes(sample):
+    # Get front camera data:
+    cam_front_data_f = nusc.get('sample_data', sample['data'][sensor])
+    data_path_f, boxes, camera_intrinsic = nusc.get_sample_data(cam_front_data_f['token'], box_vis_level=BoxVisibility.ANY)
+    print("Accessed data for ground truth boxes")
+    # pdb.set_trace()
+    boxes_gt_pixels = box_nusc(boxes, camera_intrinsic)
+    boxes_gt = []
+    for pixel_box in boxes_gt_pixels:
+        min_x = pixel_box[0]
+        max_y = pixel_box[3]
+        width_x = pixel_box[2] - pixel_box[0]
+        width_y = pixel_box[3] - pixel_box[1]
+        # pdb.set_trace()
+        boxes_gt.append([min_x, max_y, width_x, width_y])
+    return boxes_gt_pixels, boxes_gt, boxes, data_path_f, cam_front_data_f
+
+# Compare boxes:
+def compare_boxes(boxes_gt, boxes_yolo, boxes_gt_pixels):
+    matchings = {}
+    boxes_gt_dict = {}
+    boxes_yolo_dict = {}
+    i = 0
+    for box_gt in boxes_gt:
+        boxes_gt_dict[i] = box_gt
+        matchings[i] = []
+        i += 1
+    i= 0
+    for box_yolo in boxes_yolo:
+        boxes_yolo_dict[i] = box_yolo
+        i += 1
+
+    for box_gt_i in boxes_gt_dict.keys():
+        for box_yolo_i in boxes_yolo_dict.keys():
+            box1 = boxes_gt_dict[box_gt_i]
+            box2 = boxes_yolo_dict[box_yolo_i]
+            box_yolo = box(*box2)
+            iou = boxes_iou(box1, box2)
+            print("GT: ")
+            print(box1)
+            print("YoLo: ")
+            print(box2)
+            if iou >= 0.8:
+                matchings[box_gt_i].append(box_yolo_i)
+                print("Match found")
+    return matchings
+
+# Iterating over scenes
+print(nusc.list_sample)
+Nscenes = len(nusc.scene)
+# instantiating YoLo obkect:
+yolo = YoLo( nms_thresh, iou_thresh)
+for i in range(Nscenes):
+    scene = nusc.scene[i]
+    print("Evaluating scene {0}: ".format(i))
+    print(scene)
+    # iterating over timestamps / samples:
+    sample_token = scene['first_sample_token']
+    sample = nusc.get('sample', scene['first_sample_token'])
+    sample_number = 1
+    while sample_number <= scene['nbr_samples']:
+        # Get ground truth 2D boxes for front camera:
+        print("Getting Nuscenes ground truth 2D boxes: ")
+        boxes_gt_pixels, boxes_gt, boxes_nusc, data_path, cam_data = get_gt_boxes(sample)
+        # pdb.set_trace()
+        # Get Yolo Boxes:
+        print("Getting YoLo predicted 2D boxes: ")
+        boxes, boxes_yolo = get_yolo_boxes(yolo, data_path)
+
+        # Plot boxes on single image:
+        if PLOT:
+            fname_configs = {'scene_number': i, 'sample_number': sample_number}
+            plot_boxes_both(boxes_gt_pixels, boxes_nusc, boxes_yolo, data_path, cam_data, fname_configs)
+
+        # Compare bounding boxes:
+        compare_boxes(boxes_gt, boxes_yolo, boxes_gt_pixels)
+
+        # Update sample number:
+        next_sample_token = sample['next']
+        sample = nusc.get('sample', next_sample_token)
+        sample_number += 1
+        pdb.set_trace()
+
+# Draw 2D boxes:
+# data_path, boxes, camera_intrinsic = nusc.get_sample_data(cam_front_data_f['token'], box_vis_level=BoxVisibility.ANY)
+# ax = None
+# if ax is None:
+#     _, ax = plt.subplots(1, 1, figsize=(9, 16))
+# im = plot_nusc_bboxes_2D(data_path, camera_intrinsic, boxes, imsize, ax=ax)
---
+++ code/yolo_bboxes.py
@@ -0,0 +1,334 @@
+# Apurva Badithela
+# Script that matches YoLo bounding boxes to the ground truth nuscenes boxes
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from utils import *
+from darknet import Darknet
+import pdb
+from PIL import Image
+from nuscenes.utils.geometry_utils import view_points, box_in_image, BoxVisibility, transform_matrix
+from nuscenes.utils.data_classes import LidarPointCloud, RadarPointCloud
+from nuscenes.scripts.export_2d_annotations_as_json import post_process_coords, get_2d_boxes
+import pdb
+from nuscenes.nuscenes import NuScenes, NuScenesExplorer
+import os
+import random
+from nuscenes.utils.geometry_utils import view_points, box_in_image, BoxVisibility, transform_matrix
+from pyquaternion.quaternion import Quaternion
+from shapely.geometry import MultiPoint, box
+
+## Nuscenes dataset:
+dataroot="/Users/apurvabadithela/Documents/software/nuscenes/data/sets/nuscenes"
+nusc = NuScenes(dataroot="/Users/apurvabadithela/Documents/software/nuscenes/data/sets/nuscenes/")
+nms_thresh = 0.6
+iou_thresh = 0.4
+
+class YoLo():
+    def __init__(self, nms_thresh, iou_thresh):
+        self.nms_thresh = nms_thresh
+        self.iou_thresh = iou_thresh
+        self.m, self.class_names = self.load_yolo_nn()
+
+    ## Loading pre-defined YoLo neural net:
+    def load_yolo_nn(self):
+        cfg_file = './cfg/yolov3.cfg'
+        ### Set the location and name of the pre-trained weights file
+        weight_file = 'weights/yolov3.weights'
+        ### Set the location and name of the COCO object classes file
+        namesfile = 'data/coco.names'
+        ### Load the COCO object classes
+        class_names = load_class_names(namesfile)
+        print(class_names)
+        ### Load the network architecture
+        m = Darknet(cfg_file)
+        ### Load the pre-trained weights
+        m.load_weights(weight_file)
+        return m, class_names
+
+    ## Applying the YoLo algorithm:
+    def yolo(self, data_path):
+        plt.rcParams['figure.figsize'] = [24.0, 14.0]
+        img = cv2.imread(data_path) # Successfully reading data
+        # Convert the image to RGB
+        original_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # We resize the image to the input width and height of the first layer of the network.
+        resized_image = cv2.resize(original_image, (self.m.width, self.m.height))
+        boxes = detect_objects(self.m, resized_image, self.iou_thresh, self.nms_thresh)
+        boxes_pixels_yolo = get_pixel_bounds(original_image, boxes, self.class_names)
+        print(original_image.shape[1])
+        print(original_image.shape[0])
+
+        print("Modified pixels: ")
+        print(self.m.width)
+        print(self.m.height)
+
+        # Print the objects found and the confidence level
+        print_objects(boxes, self.class_names)
+        #Plot the image with bounding boxes and corresponding object class labels
+        plot_boxes(original_image, boxes, self.class_names, plot_labels = True)
+        return boxes, boxes_pixels_yolo
+
+
+# Get a scene corresponding to scene number scene_no from dataset:
+class Scenario:
+    def __init__(self, scenario_no):
+        self.scenario_no = scenario_no
+        self.scenario = None
+        self.prev_sample = ''
+        self.curr_sample = None
+        self.next_sample = None
+        self.token = None
+        self.nbr_samples = None
+        self.get_scenario(scene_no)
+        self.parse_scene()
+
+    def get_scenario(self):
+        N = len(nusc.list_scenes())
+        print(".... Loading scene ....")
+        if self.scenario_no <= N:
+            scenario = nusc.scene[self.scenario_no]
+            print(nusc.list_scenes()[self.scenario_no])
+        else:
+            rand_scenario_no = random.randrange(N)
+            scenario = nusc.scene[rand_scenario_no]
+            print(nusc.list_scenes()[rand_scenario_no])
+        print(scenario)
+        self.scenario = scenario
+
+    # Function to parse scene:
+    def parse_scene(self):
+        self.token = self.scenario['token']
+        self.nbr_samples = self.scenario['nbr_samples']
+        self.curr_sample = nusc.get('sample',self.scenario['first_sample_token'])
+        self.next_sample = nusc.get('sample',self.curr_sample['next'])
+
+    # Function to get sensor data:
+    def get_sensor_data(self, sensor):
+        sensor_data = nusc.get('sample_data', self.curr_sample['data'][sensor])
+        data_path, boxes_nuscenes, camera_intrinsic = nusc.get_sample_data(sensor_data['token'], box_vis_level=BoxVisibility.ANY)
+        sd_record = nusc.get('sample_data', sensor_data['token'])
+        imsize = (sd_record['width'], sd_record['height'])
+        return sensor_data, data_path, boxes_nuscenes, camera_intrinsic, imsize
+
+# Function for plotting 2D boxes in pixels:
+def plot_pixel_boxes(box, img, a, plot_labels=True, color = None):
+    # Define a tensor used to set the colors of the bounding boxes
+    colors = torch.FloatTensor([[1,0,1],[0,0,1],[0,1,1],[0,1,0],[1,1,0],[1,0,0]])
+
+    # Define a function to set the colors of the bounding boxes
+    def get_color(c, x, max_val):
+        ratio = float(x) / max_val * 5
+        i = int(np.floor(ratio))
+        j = int(np.ceil(ratio))
+
+        ratio = ratio - i
+        r = (1 - ratio) * colors[i][c] + ratio * colors[j][c]
+        return int(r * 255)
+
+    # Get the width and height of the image
+    width = img.shape[1]
+    height = img.shape[0]
+
+    # Create a figure and plot the image
+    a.imshow(img)
+
+    # Set the default rgb value to red
+    rgb = (1, 0, 0)
+    x1 = box[0]
+    y2 = box[1]
+
+    # Use the same color to plot the bounding boxes of the same object class
+    if len(box) >= 7 and class_names:
+        cls_conf = box[5]
+        cls_id = box[6]
+        classes = len(class_names)
+        offset = cls_id * 123457 % classes
+        red   = get_color(2, offset, classes) / 255
+        green = get_color(1, offset, classes) / 255
+        blue  = get_color(0, offset, classes) / 255
+
+        # If a color is given then set rgb to the given color instead
+        if color is None:
+            rgb = (red, green, blue)
+        else:
+            rgb = color
+
+    # Calculate the width and height of the bounding box relative to the size of the image.
+    width_x = box[2]
+    width_y = box[3]
+
+    rect = patches.Rectangle((x1, y2),
+                                     width_x, width_y,
+                                     linewidth = 2,
+                                     edgecolor = rgb,
+                                     facecolor = 'none')
+
+    # Draw the bounding box on top of the image
+    a.add_patch(rect)
+    # plt.show()
+
+# Plot 3D nuscenes bounding boxes and 2D YoLo bounding boxes:
+def plot_nusc_bboxes_3D(data_path, camera_intrinsic, boxes, imsize, ax=None):
+    im = cv2.imread(data_path)
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(9, 16))
+    # Printing 3D Nuscenes bounding boxes:
+    for box in boxes:
+        c = np.array(list(nusc.colormap[box.name]))/255.0
+        box.render_cv2(im, view=camera_intrinsic, normalize=True, colors=(c, c, c))
+        print(box)
+    im = cv2.resize(im, imsize)
+    ax.imshow(im)
+
+
+# Plotting 2D Yolo bounding boxes:
+def plot_yolo_bboxes_2D(im, ax, boxes_pixels_yolo):
+    print("YoLo bounding boxes")
+    for ii in range(len(boxes_pixels_yolo)):
+        plot_pixel_boxes(boxes_pixels_yolo[ii], im, ax)
+    plt.imshow(im)
+
+# Helper functions:
+def process_coords(corner_coords,imsize=(1600, 900)):
+    """
+    Get the intersection of the convex hull of the reprojected bbox corners and the image canvas, return None if no
+    intersection.
+    :param corner_coords: Corner coordinates of reprojected bounding box.
+    :param imsize: Size of the image canvas.
+    :return: Intersection of the convex hull of the 2D box corners and the image canvas.
+    """
+    polygon_from_2d_box = MultiPoint(corner_coords).convex_hull
+    img_canvas = box(0, 0, imsize[0], imsize[1])
+    # pdb.set_trace()
+    if polygon_from_2d_box.intersects(img_canvas):
+        img_intersection = polygon_from_2d_box.intersection(img_canvas)
+        intersection_coords = np.array([coord for coord in img_intersection.exterior.coords])
+
+        min_x = min(intersection_coords[:, 0])
+        min_y = min(intersection_coords[:, 1])
+        max_x = max(intersection_coords[:, 0])
+        max_y = max(intersection_coords[:, 1])
+
+        return min_x, min_y, max_x, max_y
+    else:
+        return None
+
+def draw_rect(selected_corners, color, im):
+    linewidth = 2
+    prev = selected_corners[-1]
+    for corner in selected_corners:
+        cv2.line(im,
+                 (int(prev[0]), int(prev[1])),
+                 (int(corner[0]), int(corner[1])),
+                 color, linewidth)
+        prev = corner
+
+# Get 2D Nuscenes boxes:
+def box_nusc(boxes, camera_intrinsic):
+    nusc_boxes = []
+    for box in boxes:
+        # Filter out the corners that are not in front of the calibrated sensor.
+        corners_3d = box.corners()
+        in_front = np.argwhere(corners_3d[2, :] > 0).flatten()
+        corners_3d = corners_3d[:, in_front]
+
+        # Project 3d box to 2d.
+        corner_coords = view_points(corners_3d, camera_intrinsic, True).T[:, :2].tolist()
+
+        # Keep only corners that fall within the image.
+        final_coords = process_coords(corner_coords)
+        nusc_boxes.append(final_coords)
+    return nusc_boxes
+
+# Plotting 2D bounding boxes for Nuscenes:
+def plot_nusc_bboxes_2D(data_path, camera_intrinsic, boxes, imsize, ax=None):
+    im = cv2.imread(data_path)
+
+    nusc_boxes = box_nusc(boxes, camera_intrinsic) # Getting all the nuscenes boxes in 2D projection
+
+    # Creating a new axis if none
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(9, 16))
+
+    # Plot bounding box:
+    for ii in range(len(nusc_boxes)):
+        box_3D = boxes[ii]
+        box = nusc_boxes[ii]
+        xmin = box[0]
+        ymin = box[1]
+        xmax = box[2]
+        ymax = box[3]
+        c = np.array(list(nusc.colormap[box_3D.name]))/255.0
+        colors = (c,c,c)
+        corners = [[xmin, ymax], [xmin, ymin], [xmax, ymin], [xmax, ymax]]
+        draw_rect(corners, colors[0][::-1], im)
+        print(box)
+
+    # Plot image on axis
+    im = cv2.resize(im, imsize)
+    ax.imshow(im)
+    return im
+
+# Plotting 2D Yolo bounding boxes:
+def plot_yolo_bboxes(im, ax, boxes_pixels_yolo):
+    print("YoLo bounding boxes")
+    for ii in range(len(boxes_pixels_yolo)):
+        box_ii = boxes_pixels_yolo[ii]
+        xmin = box_ii[0]
+        ymax = box_ii[1]
+        wx = box_ii[2]
+        wy = box_ii[3]
+        ymin = ymax + wy
+        xmax = xmin + wx
+        c = np.array([0.3, 0.3, 0.3])
+        colors = (c,c,c)
+        corners = [[xmin, ymax], [xmin, ymin], [xmax, ymin], [xmax, ymax]]
+        draw_rect(corners, colors[0][::-1], im)
+    plt.imshow(im)
+
+# Plot Yolo and Nuscenes boxes together:
+def plot_boxes_both(nusc_boxes, boxes, boxes_pixels_yolo, impath, data, fname_configs):
+    im = cv2.imread(impath)
+    sd_record = nusc.get('sample_data', data['token'])
+    imsize = (sd_record['width'], sd_record['height'])
+    ax = None
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=(9, 16))
+
+    # Show image.
+    # ax.imshow(im)
+    for ii in range(len(nusc_boxes)):
+        box_3D = boxes[ii]
+        # pdb.set_trace()
+        box = nusc_boxes[ii]
+        xmin = box[0]
+        ymin = box[1]
+        xmax = box[2]
+        ymax = box[3]
+        co = np.array(list(nusc.colormap[box_3D.name]))/255.0
+        colors = (co,co,co)
+        corners = [[xmin, ymax], [xmin, ymin], [xmax, ymin], [xmax, ymax]]
+        draw_rect(corners, colors[0][::-1], im)
+        print(box)
+
+    # Render.
+    name = "Localization"
+    im = cv2.resize(im, imsize)
+    ax.imshow(im)
+
+    print("YoLo bounding boxes")
+    for ii in range(len(boxes_pixels_yolo)):
+        plot_pixel_boxes(boxes_pixels_yolo[ii], im, ax)
+
+    # Saving figures in imglib
+    cwd = os.getcwd()
+    dirname = cwd + "/imglib/scene_"+str(fname_configs['scene_number'])
+    # pdb.set_trace()
+    if os.path.exists(dirname) is False:
+        os.mkdir(dirname)
+    fname = dirname + "/sample_no_{0}.pdf".format(fname_configs['sample_number'])
+    plt.savefig(fname, dpi='figure', format='pdf')
+    # plt.imshow(im)
+    # print(imsize)
